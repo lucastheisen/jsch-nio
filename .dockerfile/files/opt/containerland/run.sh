6bf99918758e8a487dec53400f80e5b87e47f610
Lucas Theisen <lucastheisen@pastdev.com>
2018-03-11 13:18:48 -0400

    lets check the container startup log

--- .dockerfile/files/opt/containerland/run.sh
+++ .dockerfile/files/opt/containerland/run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+echo "Creating user $SSH_USERNAME"
 useradd $SSH_USERNAME
+
+echo "Setting password for user $SSH_USERNAME"
 echo -e "$SSH_PASSWORD\n$SSH_PASSWORD" | (passwd --stdin $SSH_USERNAME)
+
+echo "Configuring .ssh for $SSH_USERNAME"
 su -l $SSH_USERNAME -c "ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ''"
 su -l $SSH_USERNAME -c "cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys"
 su -l $SSH_USERNAME -c "echo \"localhost $(cat /etc/ssh/ssh_host_rsa_key.pub)\" > ~/.ssh/known_hosts"
 su -l $SSH_USERNAME -c "chmod 600 ~/.ssh/authorized_keys ~/.ssh/known_hosts"
+
+echo "Starging sshd..."
 exec /usr/sbin/sshd -D
